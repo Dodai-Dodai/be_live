e4e2ba4ec5f497baaabfb3ae57410750ac985142
quantumshiro <hiro.1nakanishi@gmail.com>
2024-06-22 20:40:09 +0900

    add: header

--- my-app/src/pages/Home.tsx
+++ my-app/src/pages/Home.tsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import { Box, Text, Button } from '@yamada-ui/react';
 import { Link } from 'react-router-dom';
+import Be_live from '../component/header';
 
 const Home: React.FC = () => {
     
     return (
         // home画面: このサイトの説明を記載
         <div>
+            <Be_live />
             <Text fontSize="2xl" color="blue.500">Welcome to the Yamada-UI</Text>
             <Text fontSize="2xl" color="blue.500">This is a simple UI library for React</Text>
             <Text fontSize="2xl" color="blue.500">You can use this library to create a simple web application</Text>
