35e6cb94d8d84bf1c859e4f8319246129ff2e311
quantumshiro <hiro.1nakanishi@gmail.com>
2024-06-24 01:00:29 +0900

    fix: ip addr

--- src/pages/cast.tsx
+++ src/pages/cast.tsx
@@ -20,7 +20,7 @@ const MergedComponent: React.FC = () => {
 
     useEffect(() => {
         const peerInstance = new Peer('client', {
-            host: '15.168.12.232',
+            host: '15.168.173.52',
             port: 9000,
             path: '/'
         });
